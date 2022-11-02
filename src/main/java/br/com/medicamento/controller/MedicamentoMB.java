4d7c8907eda4c55f065caf348039ac0a7cbea36a
Wesley Maia <maia.weol@gmail.com>
2022-11-01 19:08:13 -0700

    DEV12: refactoring medicamento bean

--- src/main/java/br/com/medicamento/controller/MedicamentoMB.java
+++ src/main/java/br/com/medicamento/controller/MedicamentoMB.java
@@ -1,6 +1,7 @@
 package br.com.medicamento.controller;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -21,43 +22,88 @@ public class MedicamentoMB implements Serializable {
 
 	@Inject
 	private Medicamento medicamento;
-
 	@Inject
 	private MedicamentoService service;
+
+	private String apresentacao;
+	private String laboratorio;
+	private String nome;
+	private BigDecimal preco;
+	private String registro;
 
 	private List<Medicamento> medicamentos;
 
 	@PostConstruct
-	public void carregar() {
-		medicamentos = service.todosOsMedicamentos();
+	public void init() {
+		limpaCampos();
+		carregaCampos();
 	}
 
 	public void adicionar() {
 		try {
+			if (medicamento == null) {
+				medicamento = new Medicamento();
+			}
+
+			carregaEntidade();
 			service.salvar(medicamento);
-
-			medicamento = new Medicamento();
-
-			carregar();
-
+			carregaCampos();
+			limpaCampos();
 			Message.info("Salvo com sucesso");
 		} catch (NegocioException e) {
 			Message.erro(e.getMessage());
 		}
 	}
 
+	private void carregaCampos() {
+		medicamentos = service.todosOsMedicamentos();
+	}
+
+	private void carregaEntidade() {
+		medicamento.setNome(nome);
+		medicamento.setApresentacao(apresentacao);
+		medicamento.setLaboratorio(laboratorio);
+		medicamento.setPreco(preco);
+		medicamento.setRegistro(registro);
+	}
+
+	public void editar() {
+		apresentacao = medicamento.getApresentacao();
+		laboratorio = medicamento.getLaboratorio();
+		nome = medicamento.getNome();
+		preco = medicamento.getPreco();
+		registro = medicamento.getRegistro();
+	}
+
 	public void excluir() {
 		try {
 			service.remover(medicamento);
-
-			carregar();
-
+			carregaCampos();
 			Message.info(medicamento.getNome() + " foi removido");
 		} catch (NegocioException e) {
 			Message.erro(e.getMessage());
 		}
 	}
 
+	public void novo() {
+		try {
+			medicamento = new Medicamento();
+			limpaCampos();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void limpaCampos() {
+		medicamento = null;
+		apresentacao = "";
+		laboratorio = "";
+		nome = "";
+		preco = null;
+		registro = "";
+	}
+
+	// Getters and Setters
 	public Medicamento getMedicamento() {
 		return medicamento;
 	}
@@ -72,6 +118,46 @@ public class MedicamentoMB implements Serializable {
 
 	public void setMedicamentos(List<Medicamento> medicamentos) {
 		this.medicamentos = medicamentos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getApresentacao() {
+		return apresentacao;
+	}
+
+	public void setApresentacao(String apresentacao) {
+		this.apresentacao = apresentacao;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public String getRegistro() {
+		return registro;
+	}
+
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 
 }
